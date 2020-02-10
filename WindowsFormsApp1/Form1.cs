666ec7f59f9986b519634c9b49d7230f34e7a6c5
longclaw <longclaw@DESKTOP-OE9OJ1U>
2020-02-10 14:24:47 +0200

    added graphics and basic functionality

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Button[,] buttons;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons = new[,] {
+                { button1, button2, button3, button4, button5, button6, button7, button8, button9, }
+                , { button10, button11, button12, button13, button14, button15, button16, button17, button18, }
+                , { button19, button20, button21, button22, button23, button24, button25, button26, button27, }
+                , { button28, button29, button30, button31, button32, button33, button34, button35, button36, }
+                , { button37, button38, button39, button40, button41, button42, button43, button44, button45, }
+                , { button46, button47, button48, button49, button50, button51, button52, button53, button54, }
+                , { button55, button56, button57, button58, button59, button60, button61, button62, button63, }
+                , { button64, button65, button66, button67, button68, button69, button70, button71, button72, }
+                , { button73, button74, button75, button76, button77, button78, button79, button80, button81, }
+            };
+
+
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    buttons[i, j].Text = Table.matrix1[i, j].ToString();
+                    buttons[i, j].Click += new EventHandler(EventClick);
+                    buttons[i, j].KeyDown += new KeyEventHandler(EventKeyPressed);
+                }
+        }
+
+        private void EventClick(object sender, EventArgs e)
+        { 
+
+        }
+
+        public void EventKeyPressed(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode >= Keys.D1 && e.KeyCode <= Keys.D9)
+            {
+                Sudoku.CheckCorrect_Replace(Table.matrix1, e.KeyCode.ToString()[1], (Button)sender, buttons);
+                //Table.set_box((Button)sender, e.KeyCode.ToString()[1]);
+            }
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Sudoku.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms; 
+
+namespace WindowsFormsApp1
+{
+    public class Sudoku
+    {
+        public struct coords
+        {
+           public int i, j;
+        };
+
+        //method that receives a button and returns its coordinates
+        //within the given matrix
+        public static coords GetCoordinates(Button btn, Button[,] btn_array)
+        {
+            coords coordinates = new coords();
+            for (coordinates.i = 0; coordinates.i < 9; coordinates.i++)
+                for (coordinates.j = 0; coordinates.j < 9; coordinates.j++)
+                {
+                    if (btn_array[coordinates.i, coordinates.j] == btn)
+                        return coordinates;
+                }
+
+            //impossible case 
+            //MessageBox.Show(coordinates.i.ToString(), coordinates.j.ToString());
+            return coordinates; 
+        }
+
+        public static bool Checking(coords coordinates, string text, Button[,] array_btn)
+        {
+            bool row = true, collumn = true, square = true;
+            //checking row
+            for (int k = 0; k < 9; k++)
+                if (coordinates.j != k)
+                    if (array_btn[coordinates.i, k].Text == text)
+                        row = false;
+
+            //checking collumn
+            for (int k = 0; k < 9; k++)
+                if (coordinates.i != k)
+                    if (array_btn[k,coordinates.j].Text == text)
+                        collumn = false;
+
+            //checking square
+            //getting the position of the square
+            int ii = (coordinates.i / 3) * 3;
+            int jj = (coordinates.j / 3) * 3;
+            for (int p = ii; p < ii + 3; p++)
+                for (int q = jj; q < jj + 3; q++)
+                    if(coordinates.i != ii && coordinates.j != jj)
+                        if (array_btn[p, q].Text == text)
+                            square = false;
+
+            return square & collumn & row;
+        }
+
+        //method that checks if the number is modifiable or not(if it
+        //was already in the matrix before this)
+        public static bool ModifiabilityCheck(coords coordinates, int [,] matrix)
+        {
+            if (matrix[coordinates.i, coordinates.j] != 0)
+                return false;
+            return true;
+        }
+
+
+        //method that check is the number you entered is correct or not
+        //and handles the cases
+        public static void CheckCorrect_Replace(int[,] grid, char input, Button btn, Button[,] array_btn)
+        {
+            int number = input - '0'; //converting the char input into a working number
+            bool correct, modifiability;
+            coords coordinates = new coords();
+
+            coordinates = GetCoordinates(btn, array_btn);
+            correct = Checking(coordinates, input.ToString(), array_btn);
+            modifiability = ModifiabilityCheck(coordinates, grid);
+
+            if (correct && modifiability)
+            {
+                array_btn[coordinates.i, coordinates.j].Text = input.ToString();
+                //grid[coordinates.i, coordinates.j] = number;
+            }
+
+            if (modifiability == false)
+                MessageBox.Show("Please select a valid area ");
+            else
+            {
+                if (!correct)
+                    MessageBox.Show("Number you entered is not correct, try again");
+            }
+        }
+    }
+}
